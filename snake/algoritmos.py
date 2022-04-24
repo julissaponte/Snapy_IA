14fd993f85f0ca54e842a2c099c4fe289b4e97ab
BrigitteMendez1302 <U201914955@upc.edu.pe>
2022-04-23 23:54:29 -0500

    add: more fixes

--- snake/algoritmos.py
+++ snake/algoritmos.py
@@ -1,10 +1,10 @@
-from juegoui import Game
+from juegoui import Juego
 from constantes import DERECHA, IZQUIERDA, ARRIBA, ABAJO
 
 
-class HillClimbing(Game):
+class HillClimbing(Juego):
     def __init__(self, game_type):
-        Game.__init__(self, game_type)
+        Juego.__init__(self, game_type)
 
     def calcular_heuristica(self, point):
         return abs(self.food.x - point.x) + abs(self.food.y - point.y)
@@ -13,8 +13,8 @@ class HillClimbing(Game):
         vecinos = []
         direcciones = [IZQUIERDA, DERECHA, ARRIBA, ABAJO]
         for direccion in direcciones:
-            vecino = self.get_next_head(direccion)
-            if not self.detect_random_point_collision(vecino, 0):
+            vecino = self.obtener_siguiente_cabeza(direccion)
+            if not self.detectar_colision_random(vecino, 0):
                 vecino.h = self.calcular_heuristica(vecino)
                 vecinos.append((vecino, direccion))
         if vecinos:
@@ -25,9 +25,9 @@ class HillClimbing(Game):
         return None
 
 
-class AStar(Game):
+class AStar(Juego):
     def __init__(self, game_type):
-        Game.__init__(self, game_type)
+        Juego.__init__(self, game_type)
         self.open = [self.head]
         self.closed = []
         self.generar_camino()
@@ -44,37 +44,37 @@ class AStar(Game):
             self.open = [self.open[i] for i in range(len(self.open)) if not self.open[i] == actual]
             self.closed.append(actual)
             if actual == self.food:
-                while actual.origin:
+                while actual.origen:
                     self.path.append(actual)
-                    actual = actual.origin
+                    actual = actual.origen
                 return
-            actual.generate_neighbors()
-            for neighbor in actual.neighbors:
-                if neighbor not in self.obstacles and neighbor not in self.snake:
+            actual.generar_vecinos()
+            for vecino in actual.vecinos:
+                if vecino not in self.obstacles and vecino not in self.snake:
                     g_temp = actual.g + 1
-                    if neighbor not in self.open and neighbor not in self.closed:
-                        neighbor.h = self.calcular_heuristica(neighbor)
-                        neighbor.g = g_temp
-                        neighbor.f = neighbor.g + neighbor.h
-                        neighbor.origin = actual
-                        self.open.append(neighbor)
+                    if vecino not in self.open and vecino not in self.closed:
+                        vecino.h = self.calcular_heuristica(vecino)
+                        vecino.g = g_temp
+                        vecino.f = vecino.g + vecino.h
+                        vecino.origen = actual
+                        self.open.append(vecino)
                     else:
-                        if neighbor in self.open:
-                            old_neighbor = [x for x in self.open if x == neighbor][0]
-                            if old_neighbor.g > g_temp:
-                                old_neighbor.h = self.calcular_heuristica(neighbor)
-                                old_neighbor.g = g_temp
-                                old_neighbor.f = neighbor.g + neighbor.h
-                                old_neighbor.origin = actual
+                        if vecino in self.open:
+                            old_vecino = [x for x in self.open if x == vecino][0]
+                            if old_vecino.g > g_temp:
+                                old_vecino.h = self.calcular_heuristica(vecino)
+                                old_vecino.g = g_temp
+                                old_vecino.f = vecino.g + vecino.h
+                                old_vecino.origen = actual
 
-                        elif neighbor in self.closed:
-                            old_neighbor = [x for x in self.closed if x == neighbor][0]
-                            if old_neighbor.g > g_temp:
-                                old_neighbor.h = self.calcular_heuristica(neighbor)
-                                old_neighbor.g = g_temp
-                                old_neighbor.f = neighbor.g + neighbor.h
-                                old_neighbor.origin = actual
+                        elif vecino in self.closed:
+                            old_vecino = [x for x in self.closed if x == vecino][0]
+                            if old_vecino.g > g_temp:
+                                old_vecino.h = self.calcular_heuristica(vecino)
+                                old_vecino.g = g_temp
+                                old_vecino.f = vecino.g + vecino.h
+                                old_vecino.origen = actual
                                 self.closed = [self.closed[i] for i in range(len(self.closed)) if
-                                               not self.closed[i] == old_neighbor]
-                                self.open.append(old_neighbor)
+                                               not self.closed[i] == old_vecino]
+                                self.open.append(old_vecino)
         self.path = []
--- snake/juegoui.py
+++ snake/juegoui.py
@@ -6,15 +6,15 @@ from constantes import ANCHO, ALTO, TAMANIO_BLOQUE, OBSTACULOS_LIMITE, AUTO_VELO
     A_STAR_ALGORITMO, HILL_CLIMBING_ALGORITMO, BLANCO, ROJO, AZUL, MARRON, VERDE, DERECHA, IZQUIERDA, ARRIBA, ABAJO
 
 
-class Point:
+class Punto:
     def __init__(self, x, y):
         self.x = x
         self.y = y
         self.f = 0
         self.g = 0
         self.h = 0
-        self.neighbors = []
-        self.origin = None
+        self.vecinos = []
+        self.origen = None
 
     def __eq__(self, point):
         return self.__class__ == point.__class__ and self.x == point.x and self.y == point.y
@@ -22,34 +22,34 @@ class Point:
     def plot(self, display, color):
         pygame.draw.rect(display, color, pygame.Rect(self.x, self.y, TAMANIO_BLOQUE, TAMANIO_BLOQUE))
 
-    def get_direction(self):
-        if self.x == self.origin.x and self.y < self.origin.y:
+    def obtener_direccion(self):
+        if self.x == self.origen.x and self.y < self.origen.y:
             return ARRIBA
-        elif self.x == self.origin.x and self.y > self.origin.y:
+        elif self.x == self.origen.x and self.y > self.origen.y:
             return ABAJO
-        elif self.x < self.origin.x and self.y == self.origin.y:
+        elif self.x < self.origen.x and self.y == self.origen.y:
             return IZQUIERDA
-        elif self.x > self.origin.x and self.y == self.origin.y:
+        elif self.x > self.origen.x and self.y == self.origen.y:
             return DERECHA
 
-    def generate_neighbors(self):
+    def generar_vecinos(self):
         if self.x > 0:
-            self.neighbors.append(Point(self.x - TAMANIO_BLOQUE, self.y))
+            self.vecinos.append(Punto(self.x - TAMANIO_BLOQUE, self.y))
         if self.y > 0:
-            self.neighbors.append(Point(self.x, self.y - TAMANIO_BLOQUE))
+            self.vecinos.append(Punto(self.x, self.y - TAMANIO_BLOQUE))
         if self.x < ANCHO - TAMANIO_BLOQUE:
-            self.neighbors.append(Point(self.x + TAMANIO_BLOQUE, self.y))
+            self.vecinos.append(Punto(self.x + TAMANIO_BLOQUE, self.y))
         if self.y < ALTO - TAMANIO_BLOQUE:
-            self.neighbors.append(Point(self.x, self.y + TAMANIO_BLOQUE))
+            self.vecinos.append(Punto(self.x, self.y + TAMANIO_BLOQUE))
 
 
-class Game:
+class Juego:
     def __init__(self, game_type, width=ANCHO, height=ALTO):
-        self.game_type = game_type
+        self.tipo_juego = game_type
         self.width = width
         self.height = height
         self.direction = ARRIBA
-        self.head = Point(self.width / 2, self.height / 2)
+        self.head = Punto(self.width / 2, self.height / 2)
         self.snake = [self.head]
         self.score = 0
         self.obstacles = []
@@ -62,41 +62,41 @@ class Game:
         self.display = pygame.display.set_mode((self.width, self.height))
         self.font = pygame.font.SysFont('arial', 25)
         self.clock = pygame.time.Clock()
-        pygame.display.set_caption('Snake Game')
+        pygame.display.set_caption('Snake Juego')
 
         # Obstaculos
-        self.generate_obstacles()
+        self.generar_obstaculos()
         # Comida
-        self.generate_food()
+        self.generar_comida()
 
     def reset(self):
         self.direction = ARRIBA
-        self.head = Point(self.width / 2, self.height / 2)
+        self.head = Punto(self.width / 2, self.height / 2)
         self.snake = [self.head]
         self.score = 0
         self.obstacles = []
         self.food = None
         self.elapsed_times = []
         self.scores = []
-        self.generate_obstacles()
-        self.generate_food()
+        self.generar_obstaculos()
+        self.generar_comida()
 
-    def generate_food(self):
+    def generar_comida(self):
         x = random.randint(0, (self.width - TAMANIO_BLOQUE) // TAMANIO_BLOQUE) * TAMANIO_BLOQUE
         y = random.randint(0, (self.height - TAMANIO_BLOQUE) // TAMANIO_BLOQUE) * TAMANIO_BLOQUE
-        self.food = Point(x, y)
+        self.food = Punto(x, y)
         if self.food in self.snake or self.food in self.obstacles:
-            self.generate_food()
+            self.generar_comida()
 
-    def generate_obstacles(self):
+    def generar_obstaculos(self):
         for _ in range(0, OBSTACULOS_LIMITE):
             x = random.randint(0, (self.width - TAMANIO_BLOQUE) // TAMANIO_BLOQUE) * TAMANIO_BLOQUE
             y = random.randint(0, (self.height - TAMANIO_BLOQUE) // TAMANIO_BLOQUE) * TAMANIO_BLOQUE
-            obstacle = Point(x, y)
+            obstacle = Punto(x, y)
             if obstacle not in self.snake:
                 self.obstacles.append(obstacle)
 
-    def get_next_head(self, direction):
+    def obtener_siguiente_cabeza(self, direction):
         x = self.head.x
         y = self.head.y
         if direction == DERECHA:
@@ -107,9 +107,9 @@ class Game:
             y += TAMANIO_BLOQUE
         elif direction == ARRIBA:
             y -= TAMANIO_BLOQUE
-        return Point(x, y)
+        return Punto(x, y)
 
-    def detect_collision(self):
+    def detectar_colision(self):
         if self.head.x > self.width - TAMANIO_BLOQUE or self.head.x < 0 or self.head.y > self.height - TAMANIO_BLOQUE or self.head.y < 0:
             return True
         if self.head in self.snake[1:]:
@@ -117,7 +117,7 @@ class Game:
         if self.head in self.obstacles:
             return True
 
-    def detect_random_point_collision(self, point, start=1):
+    def detectar_colision_random(self, point, start=1):
         if point.x > self.width - TAMANIO_BLOQUE or point.x < 0 or point.y > self.height - TAMANIO_BLOQUE or point.y < 0:
             return True
         if point in self.snake[start:]:
@@ -125,7 +125,7 @@ class Game:
         if point in self.obstacles:
             return True
 
-    def update_ui(self):
+    def actualizar_ui(self):
         self.display.fill(VERDE)
         for point in self.snake:
             point.plot(self.display, AZUL)
@@ -140,7 +140,7 @@ class Game:
     def generar_camino(self):
         pass
 
-    def generate_plot(self):
+    def generar_plot(self):
         total_time = 0
         quantity = 0
         for elapsed_time in self.elapsed_times:
@@ -151,7 +151,7 @@ class Game:
         at = "Tiempo promedio: " + str(round(avg_time, 3))
 
         fig, ax = plt.subplots(figsize=(6, 5))
-        ax.plot(self.scores, self.elapsed_times, label=self.game_type)
+        ax.plot(self.scores, self.elapsed_times, label=self.tipo_juego)
         ax.set_title("Gráfico del tiempo que tarda el algoritmo en aumentar su puntaje")
         ax.set_xlabel("Puntaje")
         ax.set_ylabel("Tiempo en (s)")
@@ -171,17 +171,17 @@ class Game:
                 transform=ax.transAxes)
         plt.show()
 
-    def save_plot_data(self):
+    def guardar_datos_plot(self):
         actual = time.time()
         self.elapsed_times.append(actual - self.start)
         self.scores.append(self.score)
         self.start = time.time()
 
-    def single_step_traversal(self):
+    def solo_uno(self):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    self.generate_plot()
+                    self.generar_plot()
                     pygame.quit()
                     quit()
 
@@ -190,52 +190,52 @@ class Game:
                 return self.score
 
             # Move snake
-            self.head = self.get_next_head(self.direction)
+            self.head = self.obtener_siguiente_cabeza(self.direction)
             self.snake.insert(0, self.head)
 
-            if self.detect_collision():
+            if self.detectar_colision():
                 return self.score
 
             elif self.head == self.food:
                 self.score += 1
-                self.save_plot_data()
-                self.generate_food()
+                self.guardar_datos_plot()
+                self.generar_comida()
             else:
                 self.snake.pop()
 
-            self.update_ui()
+            self.actualizar_ui()
             self.clock.tick(AUTO_VELOCIDAD)
 
-    def multi_step_traversal(self):
+    def multiple(self):
         while self.path:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    self.generate_plot()
+                    self.generar_plot()
                     pygame.quit()
                     quit()
 
-            self.direction = self.path.pop(-1).get_direction()
-            self.head = self.get_next_head(self.direction)
+            self.direction = self.path.pop(-1).obtener_direccion()
+            self.head = self.obtener_siguiente_cabeza(self.direction)
             self.snake.insert(0, self.head)
 
-            if self.detect_collision():
+            if self.detectar_colision():
                 return self.score
             elif self.head == self.food:
                 self.score += 1
-                self.save_plot_data()
-                self.generate_food()
+                self.guardar_datos_plot()
+                self.generar_comida()
                 self.generar_camino()
             else:
                 self.snake.pop()
 
-            self.update_ui()
+            self.actualizar_ui()
             self.clock.tick(AUTO_VELOCIDAD)
         return self.score
 
     def main(self):
-        if self.game_type in HILL_CLIMBING_ALGORITMO:
-            return self.single_step_traversal()
-        elif self.game_type in A_STAR_ALGORITMO:
-            return self.multi_step_traversal()
+        if self.tipo_juego in HILL_CLIMBING_ALGORITMO:
+            return self.solo_uno()
+        elif self.tipo_juego in A_STAR_ALGORITMO:
+            return self.multiple()
         else:
-            raise Exception(f"Invalid algorithm: {self.game_type}!")
+            raise Exception(f"Invalid algorithm: {self.tipo_juego}!")
